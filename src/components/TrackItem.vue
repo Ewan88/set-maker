eb411fdcb27960f8a4b2accb76e384277308fec3
Ewan <baileyewan@gmail.com>
2023-06-10 11:54:15 +0100

    adds non-functional play button

--- src/components/TrackItem.vue
+++ src/components/TrackItem.vue
@@ -2,6 +2,11 @@
 	import { defineComponent, type PropType } from 'vue';
 
 	export default defineComponent({
+		data() {
+			return {
+				isHovering: false,
+			};
+		},
 		props: {
 			track: {
 				type: Object as PropType<Track>,
@@ -22,10 +27,19 @@
 <template>
 	<div class="track-item-wrapper">
 		<div class="image-wrapper">
-			<img
-				v-if="track.album.images.length > 0"
-				:src="track.album.images[track.album.images.length - 1].url"
-			/>
+			<div
+				class="track-image"
+				@mouseenter="isHovering = true"
+				@mouseleave="isHovering = false"
+			>
+				<div class="play-icon" v-if="isHovering">
+					<i class="material-icons">play_arrow</i>
+				</div>
+				<img
+					v-if="track.album.images.length > 0"
+					:src="track.album.images[track.album.images.length - 1].url"
+				/>
+			</div>
 			<div class="name-wrapper">
 				<div>{{ track.name }}</div>
 				<div class="artist-wrapper">
@@ -41,7 +55,7 @@
 	</div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 	.image-wrapper {
 		flex: 1;
 		max-width: calc(100% - 5rem);
@@ -49,11 +63,36 @@
 		align-items: center;
 	}
 
+	.track-image {
+		position: relative;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		&:hover {
+			cursor: pointer;
+		}
+
+		.play-icon {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			z-index: 2;
+
+			i.material-icons {
+				color: whitesmoke;
+				font-size: 2rem;
+			}
+		}
+	}
+
 	img {
 		width: 45px;
 		height: 45px;
-		border-radius: 15%;
 		margin: 5px;
+		position: relative;
+		z-index: 1;
 	}
 
 	.name-wrapper {
